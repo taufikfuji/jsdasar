c95d430e8f20ed1a1ad66925a890b24a3ec1e459
taufikfuji <fujitaufik@gmail.com>
2025-07-05 11:25:10 +0700

    08-operator-aritmatika.js

---
+++ 08-operator-aritmatika.js
@@ -0,0 +1,53 @@
+/*
+
++ Pertambahan
+- Pengurangan
+* Perkalian 
+** Exponensial
+/ Pembagian
+% Sisa Bagi
+
+*/
+
+
+/*
+let result = 1+2;
+console.log("1 + 2 = " + result);
+let originalResult = result;
+
+result = result - 1;
+console.log(originalResult + " - 1" + " = " + result);
+originalResult = result;
+
+result = result * 2;
+console.log(originalResult + " * 2" +  " = " + result);
+*/
+
+//Operator Augmented Assigment
+
+/*let result = 1+2;
+console.log("1 + 2 = " + result);
+let originalResult = result;
+
+result -= 1;
+console.log(originalResult + " - 1" + " = " + result);
+originalResult = result;
+
+result *= 2;
+console.log(originalResult + " * 2" +  " = " + result);
+*/
+
+//Operator Unary
+let result = +1;
+console.log( result);
+
+
+result--;
+console.log(result);
+
+
+result++;
+console.log(result);
+
+result = -result
+console.log(result);
