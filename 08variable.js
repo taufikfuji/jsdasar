695471ba5c62105eee97027e255bc4a17276269f
taufikfuji <fujitaufik@gmail.com>
2025-07-05 10:59:33 +0700

    08 variable

--- 08variable.js
+++ 08variable.js
@@ -1,11 +1,14 @@
 /* 
 Variable adalah tempat untuk menyimpan data.
 Dengan menyimpan data di variable kita bisa menggunakan lagi dengan menyebutkan nama variablenya
+
+kata kunci let digunakan untuk mengganti kata kunci var
+kata kunci const data tidak bisad rubah
 */
 
 
 
-var  fullName = "fuji taufik";
+/*var  fullName = "fuji taufik";
 var middleName = "Taufik"
 
 console.log(fullName);
@@ -18,4 +21,12 @@ console.log(fullName);
 console.log(fullName);
 
 console.log(middleName);
+*/
 
+let firstName = "Fuji Taufik";
+console.log(firstName);
+
+
+const application = "Belajar Javascript Dasar";
+//application = "Belum Belajar Javascrtip Dasar";
+console.log(application);
