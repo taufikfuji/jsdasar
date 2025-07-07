a80d2bb4dc4574f9d1ad23aac4ac81e4717d425f
taufikfuji <fujitaufik@gmail.com>
2025-07-07 23:57:06 +0700

    update

--- 12-string-template.js
+++ 12-string-template.js
@@ -8,3 +8,15 @@ console.log(template);
 const nilai = 80;
 const template2 = `Name : ${Name}, Lulus : ${nilai >70}`;
 console.log(template2);
+
+const nilai2 = 80;
+let hasil = "";
+
+if(nilai2 >= 75){
+    hasil = "Lulus";
+}else{
+    hasil = "Tidak Lulus";
+}
+
+console.log("Hasil  : " +  hasil);
+
