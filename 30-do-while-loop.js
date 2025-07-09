05c97a7cdde63a01005275cdf5e523d63e0076a7
taufikfuji <fujitaufik@gmail.com>
2025-07-09 22:14:05 +0700

    update'

---
+++ 30-do-while-loop.js
@@ -0,0 +1,15 @@
+/*
+Do while loop adalah perulangan yang mirip dengan while
+perbedaanya hanya pada pengecekan kondisi
+pengecekan kondisi di while loop dilakukan di awal sebelum perulangan dilakukan
+sedangkan di do while loop dilakukan setelah perulangan
+oleh kareta itu dalalm do while loop, minimal pasti sekali perulangan dilakukan walaupun kondisi tidak bernilai true
+
+*/
+
+let counter = 1;
+do{
+    console.log(`Perulangan Ke ${counter}`);
+    counter++
+    
+} while (counter <=10);
