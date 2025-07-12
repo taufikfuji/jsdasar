937a672894b11e0f61ef376f6c4a76c38fc06caf
taufikfuji <fujitaufik@gmail.com>
2025-07-13 00:24:58 +0700

    update

--- 34-for-of.js
+++ 34-for-of.js
@@ -7,14 +7,39 @@ For Of tidak bisa digunakan untuk melakukan iterasi data di object karena object
 */
 
 const names = ["Fuji","Taufik","Fahrul","Rahman"];
+const address = ["Karawang","Jakarta","Bandung","Jogja"]
+
+for (let i = 0; i < names.length; i++) {
+    console.log(`Hello My Name is ${names[i]} and My Address ${address[i]}`);
+    
+}
 
 for (const name of names){
     console.log(`${name}`);
     
 }
 
+
 const fullNme = "Fuji Taufik";
 for (const charater of fullNme){
     console.log(`${charater}`);
     
 }
+
+names.forEach(name=> {
+     console.log(name,"=====> Menggunakan Arrow Function dan Foreach");
+    
+})
+console.log("====================================");
+
+names.forEach((name,i)=>{
+     console.log(`Hallo Nama Saya ${name} dan tinggal di ${address[i]} =====> Menggunakan Arrow Function dan Foreach `);
+    
+})
+console.log("====================================");
+names.forEach(function(name) {
+    console.log(name,"=====> Menggunakan Function Declaration dan Foreach");
+    
+})
+
+
