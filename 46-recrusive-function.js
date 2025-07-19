110c5237dc9997c6f6cffc6c5050e2255c35062f
taufikfuji <fujitaufik@gmail.com>
2025-07-19 22:00:08 +0700

    update

--- 46-recrusive-function.js
+++ 46-recrusive-function.js
@@ -1,9 +1,32 @@
+/*
+
+    Recrusive function adalah kemampuan function untuk memanggil dirinya sendiri
+    Kadang memang ada banyak problem yang lebih mudah untuk diselesaikan menggunakan recursive function
+    contohnya adalah kasus factorial
+
+*/
+
+
+
+// Ini adalah dengan function dengan loop biasa
 function factorial(value){
-    let result = 1;
-    for(let i=1; i >= result; i++){
-        result *=1;
+    let result = 1; 
+    for (let i = 1; i <= value; i++) {
+        result *= i;
     }
-    return result;
+       return result;
+}
+console.log(factorial(7));
+
+
+
+// Ini adalah dengan recrusive function
+function factorialRecrusive(value){
+    if(value === 1){
+        return 1;
+    }else{
+        return value * factorialRecrusive(value - 1);
+    }
 }
 
-console.log(factorial(10));
+console.log(factorialRecrusive(7));
