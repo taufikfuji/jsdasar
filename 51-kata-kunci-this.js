bf2c994a61d2b464b41beec9e402e5b1d524ab58
taufikfuji <fujitaufik@gmail.com>
2025-07-25 20:52:34 +0700

    update

---
+++ 51-kata-kunci-this.js
@@ -0,0 +1,9 @@
+const peson = {
+    name : "Fuji",
+    sayHello : function (address){
+        console.log(`Hello My Name is ${this.name} from ${address}`);
+        
+    }
+}
+
+peson.sayHello("Bandung");
